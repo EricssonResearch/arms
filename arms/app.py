139087fa00c2576b92af47a0142f005916326ae7
bttner <>
2018-10-03 13:33:49 +0200

    update app, config, test settings

--- arms/app.py
+++ arms/app.py
@@ -1,7 +1,19 @@
 """Initialization of arms."""
 
+from arms.config import config
+from arms.utils import log
+
 
 def main():
+    """Main routine."""
+    initialization()
+
+
+def initialization():
     """Initialization routine."""
-    print('Hello world!')
-    return 1
+    log.__init__()
+    log.log.info("Log file initialized.")
+    if config.var.data is None:
+        log.config.warning("{}.".format(config.var.error))
+    if log.ERROR:
+        log.log.warning('Could not find settings in config.')
