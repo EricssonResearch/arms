cfd8fce7d5114e0bf3eb62a84f6d5ef9a60ee313
axelkar <axelkar@kth.se>
2018-11-16 00:24:57 +0100

    Update ArduinoMain.ino

--- arms/uart/ArduinoMain/ArduinoMain.ino
+++ arms/uart/ArduinoMain/ArduinoMain.ino
@@ -8,12 +8,23 @@ const int led = 13;
 
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
-int ID = 0;
+
+/*
+ * The variable for ID, fields and the Data-array are created with their corresponding pointers.
+ */
+int ID;
 int* pID = &ID;
 int fields;
 int* pfields = &fields;
 float Data[10];
 
+/*
+ * A test message of the type that the arduino will recieve is created.
+ * First number will be the ID.
+ * Second number will be the number of fields that the message will contain.
+ * The rest of the numbers in the message (which are separated by semi-colons) are data-values that are to be
+ * used in whatever function that is called with the ID.
+ */
 String test = "1,3,1.0;2;3.14#";
 void setup() {
   pinMode(led, OUTPUT);
@@ -26,7 +37,9 @@ void setup() {
 
 void loop() {
     
-    
+    /*
+     * Currently useless LED-code. May be used for testing later.
+     */
     if (stringComplete) {
       if (inputString[0] == '0') {
         digitalWrite(led, LOW);
@@ -43,6 +56,9 @@ void loop() {
     stringComplete = false;
     }
 
+    /*
+     * The test-string (declared up top) is used in the MSG-parser function.
+     */
     parseMSG(test);
     Serial.print(ID);
     Serial.print(",");
@@ -59,7 +75,9 @@ void loop() {
 void serSend(String s){
   
 }
-
+/*
+ * Whenever a line appears in the serial channel, a serial event is triggered.
+ */
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
@@ -77,9 +95,20 @@ void serialEvent() {
 
 
 void parseMSG(String input){
+ 
+ //A temporary string to use for parsing is created and 20 bytes gets reserved for it.
+  
  String temp = "";
  temp.reserve(20);
-  //for(int i=0, i==input.length()-1, i++){
+
+ /*
+  * A counter is initiated to be able to know where in the parsed string the parser is situated.
+  * Then a for-loop iterates untill a comma is reached, which means that the ID-number has been parsed. The pointer
+  * is referenced to the new value and temp gets reset again.
+  * 
+  * After this, the same is done for the fields-variable. Every for loop is ended with a counter++, which makes sure that the
+  * next for loop doesnt begin with parsing any char twice.
+  */
   int counter = 0;
   for(int i = counter; input[i] !=','; i++){
     temp += input[i];
@@ -95,10 +124,15 @@ void parseMSG(String input){
     counter++;
     *pfields = temp.toInt();
     temp= "";
+
+
+    /*
+     *When parsing the data-fields (separated by ";"), a while loop is used to be able to store a dynamic number of data-fields.
+     *The variable fieldCounter is used to know where to place the data-fields in Data[].
+     *The for loop which iterates through each data-field available gets "break"ed whenever the End-of-Message sign "#" is reached.
+     */
     int fieldCounter = 0;
     int len = input.length();
-//    Serial.print("HEJ");
-//    Serial.println();
     while(counter<len) {
 
     Serial.print("temp");    
